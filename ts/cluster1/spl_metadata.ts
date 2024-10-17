331cbe6abcd7ed2477aeb50453ab80a4ea3b73e2
Andre <dat.andre.on.juno@gmail.com>
2024-10-17 08:13:23 +0100

    add update metada

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,7 +1,7 @@
 import wallet from "../turbin_wallet.json";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
-import { createMetadataAccountV3, CreateMetadataAccountV3InstructionAccounts, CreateMetadataAccountV3InstructionArgs, DataV2Args } from "@metaplex-foundation/mpl-token-metadata";
+import { createMetadataAccountV3, CreateMetadataAccountV3InstructionAccounts, CreateMetadataAccountV3InstructionArgs, DataV2Args, UpdateArgs, updateMetadataAccountV2, UpdateMetadataAccountV2InstructionAccounts, UpdateMetadataAccountV2InstructionArgs } from "@metaplex-foundation/mpl-token-metadata";
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
@@ -13,9 +13,9 @@ const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
-(async () => {
+/* (async () => {
     try {
-        // Start here
+        // Creation 
         let accounts: CreateMetadataAccountV3InstructionAccounts = {
             mint: mint,
             mintAuthority: signer,
@@ -45,6 +45,48 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
         });
 
         console.log('Sending transaction...');
+        let result = await tx.sendAndConfirm(umi);
+
+        console.log(result.result);
+        console.log('resulting signature: ', bs58.encode(result.signature));
+     
+    } catch(e) {
+        console.error(`Oops, something went wrong: ${e}`)
+    }
+})(); */
+
+(async () => {
+    try {
+        // Updating 
+        let accounts: UpdateMetadataAccountV2InstructionAccounts = {
+            metadata: publicKey("DfwU4DyQRUCFcAwJiAX7Sk2Am1FXKZfHJS8tuB4sxHk6"),
+            updateAuthority: signer,
+        };
+
+        let data: DataV2Args = {
+            name: "UpCenter",
+            symbol: "UPC",
+            uri: "https://arweave.net/dkWBa-NCuNsHLFJGEhryBXQrAqKCyvQv4zpng_hhyng",
+            sellerFeeBasisPoints: 0,
+            creators: null,
+            collection: null,
+            uses: null,
+        }
+
+        let args: UpdateMetadataAccountV2InstructionArgs = {
+            data: data,
+        };
+
+        let tx = updateMetadataAccountV2(umi, {
+            ...accounts,
+            ...args
+        });
+        /*  createMetadataAccountV3(umi, {
+            ...accounts,
+            ...args
+        }); */
+
+        console.log('Sending transaction to Update...');
         let result = await tx.sendAndConfirm(umi);
 
         console.log(result.result);
